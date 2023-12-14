1dfc874e305df1d4c3d8114e296d86264912e73b
FlorianHegele <hegele.florian@gmail.com>
2023-12-14 10:24:31 +0100

    add display

--- DosRead.java
+++ DosRead.java
@@ -1,6 +1,19 @@
+import fr.dosook.StdDraw;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
+
+/*
+
+   NbrCanaux -> 1      (2 octets) : Nombre de canaux (de 1 à 6, cf. ci-dessous)
+   BitsPerSample -> 16 bits  (2 octets) : Nombre de bits utilisés pour le codage de chaque échantillon (8, 16, 24)
+   BytePerBloc -> 1 * 16 / 8 = 2 byte/bloc  (2 octets) : Nombre d'octets par bloc d'échantillonnage (c.-à-d., tous canaux confondus : NbrCanaux * BitsPerSample/8).
+   BytePerSec -> 44 100 * 2 = 88 200 byte/sec    (4 octets) : Nombre d'octets à lire par seconde (c.-à-d., Frequence * BytePerBloc).
+   Frequence -> 44 100 hz = 44.1 khZ      (4 octets) : Fréquence d'échantillonnage (en hertz)
+
+ */
 
 public class DosRead {
     static final int FP = 1000;
@@ -54,9 +67,24 @@ public class DosRead {
      * @param title the title of the window
      */
     public static void displaySig(double[] sig, int start, int stop, String mode, String title) {
-      /*
-        À compléter. Méthode a priori identique à sa version dans DosSend.
-      */
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setTitle(title);
+
+        final double xPadding = 1.01;
+        final double yPadding = 1.5;
+
+        StdDraw.setCanvasSize(1280, 700);
+        StdDraw.setYscale(min(sig) * yPadding, max(sig) * yPadding);
+        StdDraw.setXscale(-(sig.length * xPadding - sig.length), sig.length * xPadding);
+
+        if(mode.equals("line")) {
+            for(int i=1; i<sig.length; i++) {
+                final int beforePoint = i-1;
+                StdDraw.line(beforePoint, sig[beforePoint], i, sig[i]);
+            }
+        }
+
+        StdDraw.show();
     }
 
     /**
@@ -83,12 +111,22 @@ public class DosRead {
 
         // Read the audio data
         dosRead.readAudioDouble();
+        System.out.println(Arrays.toString(dosRead.audio));
+
+        displaySig(dosRead.audio, 0, dosRead.audio.length - 1, "line", "Signal audio");
+
+
         // reverse the negative values
         dosRead.audioRectifier();
+        //System.out.println(Arrays.toString(dosRead.audio));
+
         // apply a low pass filter
         dosRead.audioLPFilter(44);
+        //System.out.println(Arrays.toString(dosRead.audio));
+
         // Resample audio data and apply a threshold to output only 0 & 1
         dosRead.audioResampleAndThreshold(dosRead.sampleRate / BAUDS, 12000);
+        //System.out.println(Arrays.toString(dosRead.audio));
 
         dosRead.decodeBitsToChar();
         if (dosRead.decodedChars != null) {
@@ -96,7 +134,7 @@ public class DosRead {
             printIntArray(dosRead.decodedChars);
         }
 
-        displaySig(dosRead.audio, 0, dosRead.audio.length - 1, "line", "Signal audio");
+//        displaySig(dosRead.audio, 0, dosRead.audio.length - 1, "line", "Signal audio");
 
         // Close the file input stream
         try {
@@ -120,7 +158,7 @@ public class DosRead {
             fileInputStream.read(header);
 
             this.sampleRate = byteArrayToInt(header, 24, 32); // Frequence  (4 octets) : Fréquence d'échantillonnage
-            this.bitsPerSample = byteArrayToInt(header, 34, 32); // BytePerBloc  (2 octets) : Nombre d'octets par bloc d'échantillonnage
+            this.bitsPerSample = byteArrayToInt(header, 34, 16); // BytePerBloc  (2 octets) : Nombre d'octets par bloc d'échantillonnage
             this.dataSize = byteArrayToInt(header, 40, 32); // DataSize  (4 octets) : Nombre d'octets des données -> taille_du_fichier - taille_de_l'entête
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -135,11 +173,19 @@ public class DosRead {
      * that becomes the audio attribute
      */
     public void readAudioDouble() {
+        final int bytesPerSample = bitsPerSample / 8;
         byte[] audioData = new byte[dataSize];
+        audio = new double[dataSize / bytesPerSample];
+
         try {
             fileInputStream.read(audioData);
 
-            audio = new double[0];
+            // Assuming 16 bits per sample
+            for (int i = 0; i < audio.length; i++) {
+                final int offset = i * bytesPerSample;
+                audio[i] = (short) byteArrayToInt(audioData, offset, bitsPerSample);
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -149,9 +195,8 @@ public class DosRead {
      * Reverse the negative values of the audio array
      */
     public void audioRectifier() {
-      /*
-        À compléter
-      */
+        for (int i = 0; i < audio.length; i++)
+            if (audio[i] < 0) audio[i] = -audio[i];
     }
 
     /**
@@ -161,10 +206,18 @@ public class DosRead {
      * @param n the number of samples to average
      */
     public void audioLPFilter(int n) {
-      /*
-        À compléter
-      */
+        final double fc = (1.0 / (2.0 * n)) * sampleRate;
+        final double alpha = 2.0 * Math.PI * fc / sampleRate;
+        final double coeff = 1.0 / (1.0 + Math.sin(alpha));
+
+        for (int i = 0; i < audio.length; i++) {
+            audio[i] = (i == 0)
+                    ? audio[i]
+                    : coeff * (audio[i - 1] + audio[i] - audio[i - 1]);
+        }
     }
+
+
 
     /**
      * Resample the audio array and apply a threshold
@@ -173,9 +226,26 @@ public class DosRead {
      * @param threshold the threshold that separates 0 and 1
      */
     public void audioResampleAndThreshold(int period, int threshold) {
-      /*
-        À compléter
-      */
+        final int newLength = audio.length / period;
+        final double[] resampledAudio = new double[newLength];
+
+        for (int i = 0; i < newLength; i++) {
+            final int start = i * period;
+            final int end = Math.min((i + 1) * period, audio.length);
+
+            // Calcul de la moyenne sur la période actuelle
+            double sum = 0;
+            for (int j = start; j < end; j++)
+                sum += audio[j];
+
+
+            final double average = sum / (end - start);
+
+            // Appliquer le seuil et convertir en 0 ou 1
+            resampledAudio[i] = (average >= threshold) ? 1 : 0;
+        }
+
+        audio = resampledAudio;
     }
 
     /**
@@ -184,8 +254,47 @@ public class DosRead {
      * The next first symbol is the first bit of the first char.
      */
     public void decodeBitsToChar() {
-      /*
-        À compléter
-      */
+        int decal = -1;
+
+        for(int i=0; i<audio.length; i++) {
+            for(int j=0; j<START_SEQ.length; j++) {
+                if(audio[i] != START_SEQ[j]) break;
+
+                if(j+1 == START_SEQ.length) decal = i;
+                i++;
+            }
+        }
+
+        if (decal == -1) {
+            System.out.println("START_SEQ NOT FOUND");
+            return;
+        }
+
+        decodedChars = new char[audio.length - decal];
+        for(int i=decal; i<audio.length; i++) {
+            decodedChars[i] = (char)audio[i];
+        }
+    }
+
+    public static double min(double[] arr) {
+        double min = arr[0];
+
+        for(int i=1; i<arr.length; i++) {
+            final double x = arr[i];
+            if(min > x) min = x;
+        }
+
+        return min;
+    }
+
+    public static double max(double[] arr) {
+        double max = arr[0];
+
+        for(int i=1; i<arr.length; i++) {
+            final double x = arr[i];
+            if(max < x) max = x;
+        }
+
+        return max;
     }
 }
