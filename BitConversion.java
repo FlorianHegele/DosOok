a24f6002760bdbdd8a2a4c696a8b5be1c672dd37
FlorianHegele <hegele.florian@gmail.com>
2024-01-08 10:36:32 +0100

    update DosRead

--- BitConversion.java
+++ BitConversion.java
@@ -1,17 +1,17 @@
 public class BitConversion {
 
     // Fonction pour convertir un caractère en tableau de 8 bits
-    public static int[] charToBits(char character) {
-        int[] bits = new int[8];
+    public static byte[] charToBits(char character) {
+        byte[] bits = new byte[8];
         for (int i = 7; i >= 0; i--) {
-            bits[i] = (character & 1);
+            bits[i] = (byte) (character & 1);
             character >>= 1;
         }
         return bits;
     }
 
     // Fonction pour convertir un tableau de 8 bits en caractère
-    public static char bitsToChar(int[] bits) {
+    public static char bitsToChar(byte[] bits) {
         char character = 0;
         for (int i = 0; i < 8; i++) {
             character = (char) ((character << 1) | bits[i]);
@@ -21,7 +21,7 @@ public class BitConversion {
 
     public static void main(String[] args) {
         char myChar = '!';
-        int[] bitsArray = charToBits(myChar);
+        byte[] bitsArray = charToBits(myChar);
 
         System.out.println("Caractère: " + myChar);
         System.out.print("Bits: ");
